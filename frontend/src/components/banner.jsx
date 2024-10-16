419f030e7409220b08e80b78a9cfad0461702a31
Mohamed Shafran <shafransheikh@gmail.com>
2024-10-16 16:36:50 +0530

    temporary banner image added

--- frontend/src/components/banner.jsx
+++ frontend/src/components/banner.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
-
+import '../styles/banner.css';
 function Banner(){
-    return;
+    return(
+        <div className="image-container">
+            <img src="https://placehold.co/1070x193" alt="banner image" />
+        </div>
+    );
 }
 export default Banner;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/hero';
+import Banner from '../components/banner';
 
 function Homepage(){
     return(
     <>
         <Hero />
+        <Banner />
     </>
     );
 }
