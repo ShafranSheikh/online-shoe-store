248ebb7b896d9f142e139c3f50196fe0b0ab727b
Mohamed Shafran <shafransheikh@gmail.com>
2024-10-16 17:43:49 +0530

    featured section card and the headding added

--- frontend/src/components/featured.jsx
+++ frontend/src/components/featured.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
-
+import '../styles/featured.css';
 function Featured(){
-    return;
+    return (
+        <div className='featured-container'>
+            <h1>Featured</h1>
+            <div className="card-container">
+                <div className="card-image">
+                    <img src="https://placehold.co/250x200" alt="" />
+                </div>
+                <div className="card-content">
+                    <h2>Nike Shoes</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <button> Buy now </button>
+                </div>
+            </div>
+        </div>
+    );
 }
 export default Featured;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/hero';
 import Banner from '../components/banner';
+import Featured from '../components/featured';
 
 function Homepage(){
     return(
     <>
         <Hero />
         <Banner />
+        <Featured />
     </>
     );
 }
