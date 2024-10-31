b107e30c46bef1b961808e010c44d0dcf1aa3469
Mohamed Shafran <shafransheikh@gmail.com>
2024-10-31 05:44:12 +0530

    Men section products retrieved from the database and displayed successfully

--- frontend/src/pages/Men.jsx
+++ frontend/src/pages/Men.jsx
@@ -1,6 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import '../styles/men.css'
+const Men = ()=>{
+    const [menItem, setMenItem] = useState([]);
+    useEffect(()=>{
+        fetch("http://localhost:3000/api/Mens")
+            .then((response) => response.json())
+            .then((data) => setMenItem(data))
+            .catch((err) => console.error("Error getting Data", err));
+    },[]);
 
-function Men(){
-    return;
+    return(
+            <div className="menItem-container">
+                <div className="promo-container">
+                    <img src="https://placehold.co/1200x250" alt="" />
+                </div>
+                <div className="Itemcard-container">
+                {menItem.map((mItem, index)=>(
+                    <div className="Itemcard" key={mItem.id || index}>
+                        <img src={mItem.image} alt="" />
+                        <div className="content-container">
+                            <h2>{mItem.name}</h2>
+                            <h5>price: ${mItem.price}</h5>
+                            <div className="description-container">
+                                <p>{mItem.description}</p>
+                            </div>
+                            
+                            <div className="card-buttonContainer">
+                                <button>View Product</button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+                </div>
+            </div>
+
+    );
 }
 export default Men;
