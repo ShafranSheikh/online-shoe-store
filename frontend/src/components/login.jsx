223c44efafc42e94f2c2d04f2ac068512d8748a7
Mohamed Shafran <shafransheikh@gmail.com>
2024-11-10 22:11:03 +0530

    Added user Authentication and implemented session management

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,77 +1,56 @@
-import React from "react";
-import "../styles/login.css";
-import PersonIcon from "@mui/icons-material/Person";
-import LockIcon from '@mui/icons-material/Lock';
-import MailLockIcon from '@mui/icons-material/MailLock';
-import { Button, Checkbox, Form, Input, Flex } from "antd";
-import githubIcon from '../assets/images/github.png';
-import googleIcon from '../assets/images/google.png';
-import { Link } from 'react-router-dom'
-function Login() {
-    function onFinish(values){
-        console.log('Received values of form: ', values);
-    };
+import React,{useState} from 'react'
+import { Link,useNavigate } from 'react-router-dom';
+import '../styles/userauthentication.css';
+import axios from 'axios';
+const Login = () => {
+    const [email, setEmail] = useState('');    
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate(); 
+    const handleFormSubmit = async (e) =>{
+        e.preventDefault();
+        try{
+                const response = await axios.post("http://localhost:3000/api/auth/Login",{
+                    email,
+                    password,
+                },{withCredentials:true});
+                if(response.status === 200){
+                    navigate('/');
+                }
+                setEmail('');
+                setPassword('');
+        }catch(error){
+            console.error("Username or passwor incorrect, please try again",error);
+        }
+        
+    }
     return (
-    <div className="loginform-container">
-        <h1>Login</h1>
-        <div className="input-container">
-        <Form
-            name="login"
-            initialValues={{
-            remember: true,
-            }}
-            style={{
-            maxWidth: 360,
-            }}
-            onFinish={onFinish}
-        >
-            <Form.Item
-            name="email"
-            rules={[
-                {
-                required: true,
-                message: "Please input your email!",
-                },
-            ]}
-            >
-            <Input  placeholder="email" />
-            </Form.Item>
-            <Form.Item
-            name="password"
-            rules={[
-                {
-                required: true,
-                message: "Please input your Password!",
-                },
-            ]}
-            >
-            <Input
-                type="password"
-                placeholder="Password"
-            />
-            </Form.Item>
-            <Form.Item>
-            <Flex justify="space-between" align="center">
-                <Form.Item name="remember" valuePropName="checked"  noStyle>
-                <Checkbox>Remember me</Checkbox>
-                </Form.Item>
-                <a href="">Forgot password?</a>
-            </Flex>
-            </Form.Item>
-            <Form.Item>
-            <Button block type="primary" htmlType="submit">
-                Log in
-            </Button>
-            or <Link to= '/Signuppage'>Register now! </Link>
-            </Form.Item>
-        </Form>
-        <p>or sign in with</p>
-        <div className="social-login">
-            <button><img src={googleIcon} alt="" /></button>
-            <button><img src={githubIcon} alt="" /></button>
+        <div className="login-container" onSubmit={handleFormSubmit}>
+            <form className="login-form" >
+                <h2>Login</h2>
+                    <div className="form-group">
+                    <label htmlFor="username">Email</label>
+                    <input type="email" name="email" placeholder="Enter your email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Enter your password"value={password} onChange={(e)=>setPassword(e.target.value)}
+                    />
+                </div>
+                <button type="submit">Login</button>    
+                <div className="form-links">
+                    <p>
+                        Don't have an account? <Link to="/Signuppage">Create an account</Link>
+                    </p>
+                    <p>Or</p>
+                    <a href="/google-signin" className="google-signin">
+                        Sign in with Google
+                    </a>
+                </div>
+            </form>
         </div>
-        </div>
-    </div>
     );
 }
 export default Login;
--- frontend/src/components/signup.jsx
+++ frontend/src/components/signup.jsx
@@ -1,215 +1,48 @@
-import React from 'react';
-import { Button, Cascader, Checkbox, Col, Form, Input, Row } from 'antd';
-import '../styles/signup.css';
-
-const residences = [
-  {
-    value: 'zhejiang',
-    label: 'Zhejiang',
-    children: [
-      {
-        value: 'hangzhou',
-        label: 'Hangzhou',
-        children: [
-          {
-            value: 'xihu',
-            label: 'West Lake',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    value: 'jiangsu',
-    label: 'Jiangsu',
-    children: [
-      {
-        value: 'nanjing',
-        label: 'Nanjing',
-        children: [
-          {
-            value: 'zhonghuamen',
-            label: 'Zhong Hua Men',
-          },
-        ],
-      },
-    ],
-  },
-];
-
-const formItemLayout = {
-  labelCol: {
-    xs: {
-      span: 24,
-    },
-    sm: {
-      span: 8,
-    },
-  },
-  wrapperCol: {
-    xs: {
-      span: 24,
-    },
-    sm: {
-      span: 16,
-    },
-  },
-};
-
-const tailFormItemLayout = {
-  wrapperCol: {
-    xs: {
-      span: 24,
-      offset: 0,
-    },
-    sm: {
-      span: 16,
-      offset: 8,
-    },
-  },
-};
-
-function Signup(){
-  const [form] = Form.useForm();
-
-  const onFinish = (values) => {
-    console.log('Received values of form: ', values);
-  };
-
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import '../styles/userauthentication.css'
+ const signup = () => {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleFormSubmit = async  (e) =>{
+    e.preventDefault();
+    try{
+        const response = await axios.post("http://localhost:3000/api/auth/register",{
+          username,
+          email,
+          password,
+        },{withCredentials: true}); //Enable Cookies
+        setUsername('');
+        setEmail('');
+        setPassword('');
+    }catch(error){
+      console.error("Error Adding user",error);
+    }
+  }
   return (
-    <div className="container">
-      <div className="form-wrapper">
-        <h2 className="form-title">Register</h2>
-        <Form
-          {...formItemLayout}
-          form={form}
-          name="register"
-          onFinish={onFinish}
-          initialValues={{
-            residence: ['zhejiang', 'hangzhou', 'xihu'],
-          }}
-          scrollToFirstError
-        >
-          <Form.Item
-            name="firstName"
-            label="First Name"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your first name!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="lastName"
-            label="Last Name"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your last name!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="email"
-            label="E-mail"
-            rules={[
-              {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
-              },
-              {
-                required: true,
-                message: 'Please input your E-mail!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            label="Password"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your password!',
-              },
-            ]}
-            hasFeedback
-          >
-            <Input.Password />
-          </Form.Item>
-          <Form.Item
-            name="confirm"
-            label="Confirm Password"
-            dependencies={['password']}
-            hasFeedback
-            rules={[
-              {
-                required: true,
-                message: 'Please confirm your password!',
-              },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue('password') === value) {
-                    return Promise.resolve();
-                  }
-                  return Promise.reject(new Error('The passwords do not match!'));
-                },
-              }),
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-          {/* <Form.Item label="Captcha" extra="We must make sure that you are a human.">
-            <Row gutter={8}>
-              <Col span={12}>
-                <Form.Item
-                  name="captcha"
-                  noStyle
-                  rules={[
-                    {
-                      required: true,
-                      message: 'Please input the captcha you got!',
-                    },
-                  ]}
-                >
-                  <Input />
-                </Form.Item>
-              </Col>
-              <Col span={12}>
-                <Button>Get captcha</Button>
-              </Col>
-            </Row>
-          </Form.Item> */}
-          <Form.Item
-            name="agreement"
-            valuePropName="checked"
-            rules={[
-              {
-                validator: (_, value) =>
-                  value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
-              },
-            ]}
-            {...tailFormItemLayout}
-          >
-            <Checkbox>
-              I have read the <a href="">agreement</a>
-            </Checkbox>
-          </Form.Item>
-          <Form.Item {...tailFormItemLayout}>
-            <Button type="primary" htmlType="submit">
-              Register
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
+    <div className="signup-container">
+      <form className="signup-form"  onSubmit={handleFormSubmit}>
+        <h2>Create an Account</h2>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" placeholder="Enter the email" value={email} onChange={(e)=> setEmail(e.target.value)}/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="username">Preferred Username</label>
+          <input type="text" name="username" placeholder="Enter your username" value={username} onChange={(e)=> setUsername(e.target.value)}/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" placeholder="Enter your password" value={password} onChange={(e)=> setPassword(e.target.value)}/>
+        </div>
+        <button type="submit">Sign Up</button>
+        <div className="form-links">
+          <p>Already have an account? <Link to="/Loginpage">Login here</Link></p>
+        </div>
+      </form>
     </div>
   );
-};
-
-export default Signup;
+}
+export default signup;
